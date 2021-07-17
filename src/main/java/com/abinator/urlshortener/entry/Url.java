fc9310d87bc77d46692dafaa9d452c17c0329e8a
Personal <personal@Abhinavs-MacBook-Pro.local>
2021-07-17 12:36:47 +0530

    URL-3 : Define model for url

--- src/main/java/com/abinator/urlshortener/entry/Url.java
+++ src/main/java/com/abinator/urlshortener/entry/Url.java
@@ -1,2 +1,30 @@
-package com.abinator.urlshortener.entry;public class Url {
+package com.abinator.urlshortener.entry;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Url {
+
+    @NotNull
+    private Integer id;
+
+    @NotBlank
+    private String url;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdAt;
 }
