d32e6996ad3c43fb987d6cdf2e7e791a98e35f7c
Personal <personal@Abhinavs-MacBook-Pro.local>
2021-07-17 12:37:13 +0530

    URL-4 : Define redis configurations

--- src/main/java/com/abinator/urlshortener/config/RedisConfiguration.java
+++ src/main/java/com/abinator/urlshortener/config/RedisConfiguration.java
@@ -1,2 +1,32 @@
-package com.abinator.urlshortener.config;public class RedisConfiguration {
+package com.abinator.urlshortener.config;
+
+import com.abinator.urlshortener.entry.Url;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    RedisConnectionFactory connectionFactory;
+
+    @Bean
+    RedisTemplate<String, Url> redisTemplate() {
+        final RedisTemplate<String, Url> redisTemplate = new RedisTemplate<>();
+        Jackson2JsonRedisSerializer valueSerializer = new Jackson2JsonRedisSerializer(Url.class);
+        valueSerializer.setObjectMapper(objectMapper);
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(valueSerializer);
+        return redisTemplate;
+    }
 }
